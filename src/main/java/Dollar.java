22f7d27a6a9cea6018ba42f592d65ce790280ae4
Joe Donghee Cho <recordingbetter@gmail.com>
2017-07-27 14:36:02 +0900

    update: test codes

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -1,9 +1,12 @@
 public class Dollar {
-    public int amount = 0;
+    public int original;
+    public int amount = 10;
     public Dollar(int dollar) {
+        original = dollar;
         amount = dollar;
     }
     public void times(int multiplier) {
-        amount = amount * multiplier;
+        // amount = amount * multiplier;
+        amount = original * multiplier;
     }
 }
--- src/test/java/TestCurrency.java
+++ src/test/java/TestCurrency.java
@@ -16,4 +16,13 @@ public class TestCurrency {
         five.times(3);
         assertEquals(15, five.amount);
     }
+
+    @Test
+    public void testMultiplication3() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+        five.times(3);
+        assertEquals(15, five.amount);
+    }
 }
