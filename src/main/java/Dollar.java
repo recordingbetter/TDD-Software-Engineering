8196876e4eab38f97f4e365036e4e11e11c1d3fc
Joe Donghee Cho <recordingbetter@gmail.com>
2017-07-27 13:46:27 +0900

    add: test codes, dollar

---
+++ src/main/java/Dollar.java
@@ -0,0 +1,9 @@
+public class Dollar {
+    public int amount = 10;
+    public Dollar(int dollar) {
+
+    }
+    public void times(int multiplier) {
+
+    }
+}
---
+++ src/test/java/TestCurrency.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestCurrency {
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(3);
+        assertEquals(15, five.amount);
+    }
+}
